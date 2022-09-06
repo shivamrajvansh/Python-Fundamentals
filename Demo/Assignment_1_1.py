a5c1696831683ac0b8f6e06cffe0c32d34ea3b9a
Shivam Rajvansh <shivamrajvansh@gmail.com>
2022-09-06 13:36:44 +0530

    yes

--- Demo/Assignment_1_1.py
+++ Demo/Assignment_1_1.py
@@ -1,3 +1,8 @@
+#Wap to input 2 values in 2 variables and swap them.
+#-> with the help of 3rd variable 
+#-> without help of 3rd variable 
+#-> in one line or expression 
+
 num1=int(input("Enter number 1 : "))
 num2=int(input("Enter number 2 : "))
 num3 = num1
--- Demo/Assignment_1_2.py
+++ Demo/Assignment_1_2.py
@@ -1,3 +1,9 @@
+#Wap to input 3 digit number , reverse it and also find the sum of a digit of a number ?
+#Hint :-
+#num=123
+#rev=321
+#sum=6
+
 num=input("Enter number to reverse and sum : ")
 print(num[2],num[1],num[0])
 print("Sum of digit : ", int(num[0]) + int(num[1]) + int(num[2]))
